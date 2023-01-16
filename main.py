65ecb4f04940056d62835e6e6db7de094773a299
Pablo Zuta <pablo.zuta.h@gmail.com>
2023-01-16 17:30:23 -0400

    se agregan rutas y error handling

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import random
 # http://127.0.0.1:8000 = API endpoint
 # http://127.0.0.1:8000/docs# = esto nos da informacion sobre nuestras rutas
 
 app = FastAPI()
+
+BOOKS_FILE ="books.json"
+BOOK_DATABASE = [
+    "Deep Work",
+    "The Sun Also Rises",
+    "Infinite Jest",
+    "The Cambridge Handbook of Expertise and Expert Performance"
+]
 
 
 """ @app.get("/")
@@ -22,10 +31,28 @@ async def home():
 # /list-books
 @app.get("/list-books")
 async def list_books():
-    return{"books": []}
+    return{"books": BOOK_DATABASE}
+
 # /books-by-index/{index}
+@app.get("/books-by-index/{index}")
+async def books_by_index(index: int):
+    if index < 0 or index >= len(BOOK_DATABASE):
+        # Fail message
+        raise HTTPException(404, f"Index {index} is out of range {len(BOOK_DATABASE)}")
+    else:    
+        return{"books": BOOK_DATABASE[index]}
+
 # /get-random-book
+@app.get("/get-random-book")
+async def get_random_book():
+    return random.choice(BOOK_DATABASE)
+
 # /add-book
+@app.post("/add-book")
+async def add_book(book: str):
+    BOOK_DATABASE.append(book)
+    return {"message": f"Book {book} was added to database!"}
+
 # /get-book?id=3hf...
 
 
