a787e4fc1a1b93bb98b39841980dc2417ce58204
Pablo Zuta <pablo.zuta.h@gmail.com>
2023-01-17 03:37:17 -0400

    se terminan rutas de API

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
+from uuid import uuid4
 from fastapi import FastAPI, HTTPException
+# este modulo se usa para generar una sugerencia de libro de forma aleatoria
 import random
+import os
+import json
+from pydantic import BaseModel
+from typing import Optional, Literal
+from fastapi.encoders import jsonable_encoder
 # http://127.0.0.1:8000 = API endpoint
 # http://127.0.0.1:8000/docs# = esto nos da informacion sobre nuestras rutas
+# uvicorn main:app --reload
 
 app = FastAPI()
 
+# Book Model
+class Book(BaseModel):
+    name: str
+    price: float
+    genre: Literal["programming", "fiction", "history"]
+    book_id: Optional[str] = uuid4().hex
+
+
 BOOKS_FILE ="books.json"
-BOOK_DATABASE = [
-    "Deep Work",
-    "The Sun Also Rises",
-    "Infinite Jest",
-    "The Cambridge Handbook of Expertise and Expert Performance"
-]
+BOOK_DATABASE = []
 
-
-""" @app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/books")
-async def books():
-    return {"message": "Here are the Books"} """
+if os.path.exists(BOOKS_FILE):
+    with open(BOOKS_FILE, "r") as f:
+        BOOK_DATABASE = json.load(f)
 
 
 # / root path
@@ -49,11 +55,23 @@ async def get_random_book():
 
 # /add-book
 @app.post("/add-book")
-async def add_book(book: str):
-    BOOK_DATABASE.append(book)
-    return {"message": f"Book {book} was added to database!"}
+async def add_book(book: Book):
+    book.book_id = uuid4().hex
+    json_book = jsonable_encoder(book)
+    BOOK_DATABASE.append(json_book)
+    with open(BOOKS_FILE, "w") as f:
+        json.dump(BOOK_DATABASE, f)
+    return {"message": f"Book {book} was added to database!", "book_id": book.book_id}
 
 # /get-book?id=3hf...
+@app.post("/get-book")
+async def get_book(book_id: str):
+    for book in BOOK_DATABASE:
+        if book["book_id"] == book_id:
+            return book
+
+    raise HTTPException(404, f"Book not found: {book_id}")        
+
 
 
 
